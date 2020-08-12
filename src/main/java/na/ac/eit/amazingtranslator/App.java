796c1e194b2f347160d200e35d8f60daf385e481
Andrew Gillan <yoyoyo@gmail.com>
2020-08-12 14:00:01 +1200

    initial commit

---
+++ src/main/java/na/ac/eit/amazingtranslator/App.java
@@ -0,0 +1,14 @@
+package na.ac.eit.amazingtranslator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "yoooooooooooo" );
+    }
+}
